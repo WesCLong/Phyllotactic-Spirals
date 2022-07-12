9498a3933ccf21c33eed402600833fd6c1eb5b56
Wesley Long <33431242+WesCLong@users.noreply.github.com>
2022-07-12 16:12:41 -0400

    add the spiral file

---
+++ Spiral.py
@@ -0,0 +1,25 @@
+from turtle import*
+# speed(0)
+#  turt = turtle.Turtle()
+
+
+
+def spiral( radi , degrees ):
+    num = radi
+    num1 = degrees
+    for i in range ( 10000  ):
+        fd(i)
+        circle( i , degrees )
+
+def Phyllotactic_Spirals(left_num_spirals, right_num_spirals):
+    left[left_num_spirals]
+    right[right_num_spirals]
+
+    leftAngle = 360/left_num_spirals
+    rightAngle = 360/right_num_spirals
+
+
+
+
+    
+spiral (3,30)
